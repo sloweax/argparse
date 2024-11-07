90e6aa0ffb9bc08e3d896a2c7dd226254505ef35
sloweax <sloweax@users.noreply.github.com>
2024-11-07 14:13:31 -0300

    add {Int,String}Append()

--- types.go
+++ types.go
@@ -17,12 +17,28 @@ func String(name string, v *string) Option {
 	}}
 }
 
+func StringAppend(name string, v *[]string) Option {
+	return Option{Name: name, Nargs: 1, Callback: func(ctx *Context, args ...string) {
+		*v = append(*v, args[0])
+	}}
+}
+
 func Int(name string, v *int) Option {
 	return Option{Name: name, Nargs: 1, Callback: func(ctx *Context, args ...string) {
 		if num, err := strconv.Atoi(args[0]); err != nil {
 			ctx.AbortWithError(fmt.Errorf("%s is not an integer", args[0]))
 		} else {
 			*v = num
+		}
+	}}
+}
+
+func IntAppend(name string, v *[]int) Option {
+	return Option{Name: name, Nargs: 1, Callback: func(ctx *Context, args ...string) {
+		if num, err := strconv.Atoi(args[0]); err != nil {
+			ctx.AbortWithError(fmt.Errorf("%s is not an integer", args[0]))
+		} else {
+			*v = append(*v, num)
 		}
 	}}
 }
