9b308768427d476903f18e740d366440890b245c
sloweax <sloweax@users.noreply.github.com>
2024-11-07 23:45:49 -0300

    add example/multi-positional.go

---
+++ example/multi-positional.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sloweax/argparse"
+)
+
+// $ go run . a b -f c d
+// flag=true args=[a b c d]
+
+func main() {
+	parser := argparse.New()
+
+	flag := false
+	parser.AddOption(argparse.Bool("f", &flag))
+
+	args := make([]string, 0)
+	parser.AddOption(argparse.StringRest("args", &args))
+
+	if err := parser.ParseArgs(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("flag=%v args=%v\n", flag, args)
+}
