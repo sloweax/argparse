d7035786b35915b6c9e3e20585122110b8b75cc3
sloweax <sloweax@users.noreply.github.com>
2024-11-11 23:23:22 -0300

    update example/from-struct.go

--- example/from-struct.go
+++ example/from-struct.go
@@ -11,11 +11,11 @@ type MyStruct struct {
 	// alias accepts a comma separated list of names
 	Flag       bool   `alias:"f"`
 	LongName   string `alias:"l"`
-	Positional string `type:"positional"`
+	Positional string `type:"positional" required:"true"`
 	// if name is not specified. it will be auto generated based on field name
 	BadNameForOption string `name:"nice-name"`
 	// dont add the option below
-	Ignored string `ignore:"true"`
+	Ignored string `ignored:"true"`
 }
 
 // go run . --long-name abc -f 123 --nice-name test
